3c0dac206d10c842dfa454e71a9b8888ac397e04
Shivraj <sdkadgond9+github@gmail.com>
2024-08-18 14:16:06 +0530

    Add: Added search functionality

--- app/(www)/layout.tsx
+++ app/(www)/layout.tsx
@@ -2,6 +2,8 @@ import { Navbar } from "@/components/navbar";
 
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { CartProvider } from "@/context/cart-context";
+import { Loader2Icon } from "lucide-react";
+import { Suspense } from "react";
 
 export default async function RootLayout({
   children,
@@ -12,7 +14,15 @@ export default async function RootLayout({
     <>
       <CartProvider>
         <div className="flex flex-col h-full">
-          <Navbar />
+          <Suspense
+            fallback={
+              <div className="flex justify-center items-center h-16">
+                <Loader2Icon className="animate-spin h-8 w-8" />
+              </div>
+            }
+          >
+            <Navbar />
+          </Suspense>
           <main className="flex-grow">{children}</main>
         </div>
         <TailwindIndicator />
--- app/(www)/page.tsx
+++ app/(www)/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { ProductGrid } from "./_components/product-grid";
 import { Sidebar } from "./_components/sidebar";
+import { Loader2Icon } from "lucide-react";
 
 export default function Home() {
   const [priceRange, setPriceRange] = useState<{
@@ -18,7 +19,15 @@ export default function Home() {
     <div className="flex h-full">
       <Sidebar onPriceRangeChange={handlePriceRangeChange} />
       <div className="flex-grow overflow-auto lg:ml-[260px]">
-        <ProductGrid priceRange={priceRange} />
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center h-full">
+              <Loader2Icon className="animate-spin h-8 w-8" />
+            </div>
+          }
+        >
+          <ProductGrid priceRange={priceRange} />
+        </Suspense>
       </div>
     </div>
   );
